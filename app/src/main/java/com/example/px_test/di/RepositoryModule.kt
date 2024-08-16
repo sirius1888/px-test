3a8104d0817b5f3b56fbafc63609b03a6ffef1c3
Sergei Karukes <ser1888kg@gmail.com>
2024-08-17 00:31:45 +0300

    Добавил di для репозитория

---
+++ app/src/main/java/com/example/px_test/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.example.px_test.di
+
+import com.example.px_test.data.AuthRepository
+import com.example.px_test.data.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
